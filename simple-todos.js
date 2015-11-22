1d757725d760eab0f53ce66e5e3ab27029d187fc
mikesavior <mike@mokentrol.com>
2015-11-22 17:33:55 -0500

    my own take on the meteor task list as I need features for a note taking app.

---
+++ simple-todos.js
@@ -0,0 +1,44 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isClient){
+Template.body.events({
+    "submit .new-task": function (event) {
+      event.preventDefault();
+      var text = event.target.text.value;
+      
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()        
+      });
+      
+      event.target.text.value = "";
+    }
+});
+}
+
+if (Meteor.isClient){
+Template.task.events({
+  "click .toggle-checked": function () {
+    Tasks.update(this._id, {
+      $set: {checked: ! this.checked}
+    });
+  },
+  "click .delete": function() {
+    Tasks.remove(this._id);
+    console.log(event);
+      }
+});
+}
+
+
+
+
+
